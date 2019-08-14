fe618ea79019a640081af36ece5a2324f4b1a194
Xue <xuev@rezare.co.nz>
2019-08-15 11:52:45 +1200

    Added simple API structure

---
+++ ProjectCoffeAPI/Services/CoffeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectCoffeAPI.Services
+{
+    public class CoffeService : ICoffeService
+    {
+        private Dictionary<string, string> _CoffeListDictrionary = new Dictionary<string, string>();
+        public Dictionary<string, string> GetUserList()
+        {
+            return _CoffeListDictrionary;
+        }
+    }
+}
