8c3cac05dcf93c30c8257f2e253ce27b0517a077
REZARE\BloemJ <janine.bloem@rezare.co.nz>
2019-08-15 14:03:50 +1200

    added path to select

--- ProjectCoffeeWebUINg/src/app/app-routing.module.ts
+++ ProjectCoffeeWebUINg/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CoffeeSelectComponent } from "src/app/coffee-select/coffee-select.component";
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+   { path: 'select', component: CoffeeSelectComponent }
 ];
 
 @NgModule({
